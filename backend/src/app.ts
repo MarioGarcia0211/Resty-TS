dc536149677c095526d595dbe8949b9ebe3abf9c
MarioGarcia0211 <jhonmariogarciaballesteros@gmail.com>
2025-10-06 00:19:40 -0500

    feat(app): integrar rutas de restaurantes en la aplicación principal
    Se importó y configuró el enrutador de restaurantes en app.ts.
    
    - Se agregó la ruta base /api/restaurants.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,9 @@ import express, { Application } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
+import restaurantRoutes from "./routes/restaurant.route";
 
+// Initialize express app
 const app: Application = express();
 
 // Middlewares
@@ -11,5 +13,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Routes
+app.use("/api/restaurants", restaurantRoutes);
 
 export default app;
